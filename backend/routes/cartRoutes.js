b3c5bee0963f8fc45c056af32031698a137f92a6
IT20159030 <it20159030@my.sliit.lk>
2023-09-19 19:59:40 +0530

    changed cart controller to PUT

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.route('/').post(addCartItems);
 router.route('/:id').get(getCartById);
-router.route('/:id').patch(updateCartById);
+router.route('/:id').put(updateCartById);
 
 export default router;
