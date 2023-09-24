95d73acbba618403df2539efb37ac6cfd47b45ae
Janindu Dissanayake <87414583+JaninduDissanyake@users.noreply.github.com>
2023-09-25 02:27:41 +0530

    modified with rn-toast

--- frontend/screens/Product/ReviewScreen.jsx
+++ frontend/screens/Product/ReviewScreen.jsx
@@ -1,31 +1,72 @@
 import React, { useRef, useState } from 'react';
-import { View, Text, Image, StyleSheet, ScrollView, TouchableOpacity, TextInput } from 'react-native';
+import { useUser } from '../../context/UserContext';
+import { useNavigation } from '@react-navigation/native';
+import { useToast } from 'react-native-toast-notifications';
 import { ReviewsRatings, StarRating } from '../../components';
 import { createProductReview } from '../../services/ProductServices';
+import { View, Text, Image, StyleSheet, ScrollView, TouchableOpacity, TextInput } from 'react-native';
 
 const ReviewScreen = ({ route }) => {
+    const toast = useToast();
     const { product } = route.params;
-    const [comment, setComment] = useState('');
     const reviewItemRef = useRef(null);
+    const { user, setUser } = useUser();
+    const navigation = useNavigation();
 
     const proID = product.product;
+
+    const [name, setName] = useState('');
+    const [comment, setComment] = useState('');
+    const [rating, setRating] = useState('');
+    const [isValid, setIsValid] = useState(true);
+    const [isCommentValid, setIsCommentValid] = useState(true);
 
     const handlePostReview = async () => {
         try {
             const newReview = {
-                user: '641aaee2b8ed930c6e7186c1',
-                name: 'John Doe',
+                user: user._id,
+                name: user.name,
                 comment: comment,
-                rating: '3',
+                rating: rating,
                 productID: proID,
             };
 
             const response = await createProductReview(proID, newReview);
 
             console.log('Review created:', response);
+            toast.show('Review posted!', {
+                type: 'success',
+                duration: 3000,
+                animationType: 'zoom-in',
+                textStyle: {
+                    fontSize: 30,
+                    color: 'white',
+                },
+                containerStyle: {
+                    height: 60,
+                    paddingHorizontal: 20,
+                    backgroundColor: '#333',
+                },
+                placement: 'bottom',
+            });
+            navigation.goBack();
         } catch (error) {
             console.error('Error creating review:', error.message);
         }
+    };
+
+    const validateComment = () => {
+        const isValidCom = comment.trim() !== ''; // Check if comment is not empty
+        setIsCommentValid(isValidCom);
+
+        // Enable the button if both inputs are valid
+        setIsValid(isValidCom && !isNaN(parseFloat(rating)) && parseFloat(rating) >= 1 && parseFloat(rating) <= 5);
+    };
+
+    const validateRating = (text) => {
+        // Check if it's a number and within the range [1, 5]
+        setIsValid(isCommentValid && !isNaN(parseFloat(text)) && parseFloat(text) >= 1 && parseFloat(text) <= 5);
+        setRating(text);
     };
 
     return (
@@ -33,42 +74,71 @@ const ReviewScreen = ({ route }) => {
             <Text style={styles.name} accessibilityLabel={`${product.name} Name`} accessibilityRole="header">
                 {product.name}
             </Text>
-            <Text style={styles.price} accessibilityLabel={`${product.name} Price`} accessibilityRole="text">
+            <Text style={styles.price} accessibilityLabel={`Price ${product.price}`} accessibilityRole="text">
                 ${product.price}
             </Text>
-            <Text
+            {/* <Text
                 style={styles.description}
                 accessibilityLabel={`${product.name} Description`}
                 accessibilityRole="text"
             >
                 {product.description}
-            </Text>
+            </Text> */}
 
             {/* ReviewsRatings component */}
             <View style={styles.reviewContainer}>
                 <View style={styles.reviewHeaderSection}>
-                    <Text style={styles.reviewHeader}>Reviews</Text>
-                    <View style={styles.rightSection}>
-                        <StarRating rating={product.rating} />
-                        <Text style={styles.ratingText}>{product.numReviews} Ratings</Text>
-                    </View>
+                    <Text style={styles.reviewHeader} accessibilityLabel="Add Review" accessibilityRole="text">
+                        Add Review
+                    </Text>
                 </View>
                 {/* Write Review Input */}
                 <TextInput
-                    style={styles.input}
+                    style={[styles.input, !isCommentValid && styles.invalidInput]}
                     placeholder="Write your review..."
                     multiline
                     value={comment}
                     onChangeText={(text) => setComment(text)}
+                    onBlur={validateComment} // Validate on blur
+                    accessibilityLabel="Review Input"
+                    accessibilityHint="Please enter your review here." // Provide a hint if needed
                 />
+                {!isCommentValid && (
+                    <Text
+                        style={styles.errorMessage}
+                        accessibilityLabel="Review Input Error Message -  This field is required"
+                        accessibilityRole="text" // Assuming this is a text element
+                    >
+                        This field is required.
+                    </Text>
+                )}
 
+                <TextInput
+                    style={[styles.input, !isValid && styles.invalidInput]}
+                    placeholder="Rate product out of 5"
+                    value={rating}
+                    onChangeText={validateRating} // Validate rating on text change
+                    keyboardType="numeric"
+                    accessibilityLabel="Review Input"
+                    accessibilityHint="Please rate the product in a scale of 1 to 5."
+                />
+                {!isValid && (
+                    <Text
+                        style={styles.errorMessage}
+                        accessibilityLabel="Rate Input Error Message - Please enter a valid rating between 1 and 5"
+                        accessibilityRole="text"
+                    >
+                        Please enter a valid rating between 1 and 5.
+                    </Text>
+                )}
                 {/* Post Button */}
                 <TouchableOpacity
-                    style={styles.postBtn}
+                    style={[styles.postBtn, (!isValid || !isCommentValid) && styles.disabledBtn]}
                     onPress={handlePostReview}
                     accessible={true}
                     accessibilityRole="button"
                     accessibilityLabel="Post Review"
+                    disabled={!isValid || !isCommentValid} // Disable the button if either is not valid
                 >
                     <Text style={styles.postBtnText}>Post Review</Text>
                 </TouchableOpacity>
@@ -163,7 +233,7 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         padding: 10,
         marginBottom: 15,
-        fontSize: 16,
+        fontSize: 20,
         minHeight: 100,
     },
     postBtn: {
@@ -180,9 +250,23 @@ const styles = StyleSheet.create({
     },
     postBtnText: {
         color: '#fff',
-        fontSize: 16,
+        fontSize: 18,
         fontWeight: 'bold',
         textAlign: 'center',
+    },
+    invalidInput: {
+        borderColor: 'red',
+    },
+    errorMessage: {
+        color: 'red',
+        marginTop: 5,
+        fontSize: 20,
+        textAlign: 'center',
+        marginBottom: 15,
+    },
+    disabledBtn: {
+        backgroundColor: 'gray',
+        opacity: 0.6,
     },
 });
 
