d3b84cf514e1bc66c7808205879fd97edc31ef6e
IT20159030 <it20159030@my.sliit.lk>
2023-10-10 08:54:49 +0530

    update

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -15,6 +15,6 @@ const router = express.Router();
 router.route('/').post(registerUser).get(getUsers);
 router.post('/login', authUser);
 router.route('/account').put(updateUser);
-router.route('/:id').put(protect, admin, adminPrivileges).delete(protect, deleteUser).get(protect, getUserById);
+router.route('/:id').put(protect, admin, adminPrivileges).delete(protect, deleteUser).get(getUserById);
 
 export default router;
